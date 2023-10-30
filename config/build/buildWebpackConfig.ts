6bab9425a15238649ddab16fe8636822a22bcfda
Denis Belozerov <molotov.ant@mail.ru>
2023-10-31 03:51:30 +0500

    build fix

---
+++ config/build/buildWebpackConfig.ts
@@ -0,0 +1,27 @@
+import webpack from "webpack";
+
+import { BuildOptions } from "./types/config";
+
+import { buildLoaders } from "./buildLoaders";
+import { buildResolvers } from "./buildResolvers";
+import { buildPlugins } from "./buildPlugins";
+import { buildDevServer } from "./buildDevServer";
+
+export function buildWebpackConfig(options: BuildOptions): webpack.Configuration {
+  return {
+    mode: options.mode,
+    entry: options.paths.entry,
+    module: {
+      rules: buildLoaders(options),
+    },
+    resolve: buildResolvers(),
+    devtool: options.isDev ? "inline-source-map" : undefined,
+    devServer: options.isDev ? buildDevServer(options) : undefined,
+    output: {
+      filename: "[name].[contenthash].js",
+      path: options.paths.build,
+      clean: true,
+    },
+    plugins: buildPlugins(options.paths.html),
+  };
+}
