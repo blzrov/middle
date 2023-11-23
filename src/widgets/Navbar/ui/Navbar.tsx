aa32f4703e49c76bdc4dfc97cd0a1ecf4e13c112
Denis Belozerov <molotov.ant@mail.ru>
2023-11-24 02:13:10 +0500

    add db

--- src/widgets/Navbar/ui/Navbar.tsx
+++ src/widgets/Navbar/ui/Navbar.tsx
@@ -1,18 +1,25 @@
 import AppLink, { AppLinkTheme } from 'shared/ui/AppLink/AppLink';
 import styles from './Navbar.module.scss';
 import { classNames } from 'shared/lib/classNames/classNames';
+import { useState } from 'react';
+import Modal from 'shared/ui/Modal/Modal';
 
 interface NavbarProps {
   className?: string;
 }
 
 const Navbar = ({ className }: NavbarProps) => {
+  const [isAuthModal, setIsAuthModal] = useState(false);
+
   return (
     <div className={classNames(styles.Navbar, {}, [className])}>
       <div className={styles.links}>
-        <AppLink theme={AppLinkTheme.LINK} className={styles.mainLink} to="/">
-          Main
+        <AppLink onClick={() => setIsAuthModal(true)} theme={AppLinkTheme.LINK} className={styles.mainLink} to="/">
+          Войти
         </AppLink>
+        <Modal isOpen={isAuthModal} onClose={() => setIsAuthModal(false)}>
+                Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aliquid commodi consequatur eligendi impedit incidunt necessitatibus possimus quis saepe sunt totam.
+        </Modal>
       </div>
     </div>
   );
